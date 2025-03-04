7b7f3e81bcc77d7a7e51cc26766ae63b0c306b66
Sebastian Techstarter <sebastianrehberg.rehberg@tn.techstarter.de>
2025-03-04 14:16:44 +0100

    Add files via upload

---
+++ HA-2025-03-03/hello_world.js
@@ -0,0 +1,36 @@
+let my_name = "Sebastian";
+let alter = "37";
+
+console.log(my_name);
+console.log(alter);
+
+// Fingerübung: 
+// Definiert 2 variablen (zahlen) a und b
+// Gebt mithilfe von if-else if-else aus, welche Größer ist oder ob sie
+// gleich groß sind
+let a = 5;
+let b = 2;
+if (a>b) {
+    console.log(`${a} ist größer.`)
+}
+else if (a<b) {
+    console.log(`${b} ist größer`)
+}
+else {
+    console.log("Beide Zahlen sind gleich.")
+}
+
+
+// Fingerübung 2: 
+// Erstelle eine variable divisionResult
+// Schreibe in diese Variable das Ergebnis von a/b, aber nur,
+// wenn b nicht 0 ist. Sonst benutze console.error um einen Fehler auszugeben.
+
+// Fingerübung 3: 
+// Erstelle eine variable punkte
+// Die Bewertung läuft wie folgt:
+// Mehr als 90 Punkte: Ausgabe "Note 1"
+// Mehr als 80 Punkte: Ausgabe "Note 2"
+// Mehr als 70 Punkte: Ausgabe "Note 3"
+// Mehr als 60 Punkte: Ausgabe "Note 4"
+// Sonst: Ausgabe "Durchgefallen!"
