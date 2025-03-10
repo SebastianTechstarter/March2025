092ad5695c4f623faa83d9882a51d3f8cc26d69e
Beatbroetchen88 <sebastianrehberg.rehberg@tn.techstarter.de>
2025-03-10 17:18:14 +0100

    update

--- HA-2025-03-10/uebung.js
+++ HA-2025-03-10/uebung.js
@@ -5,7 +5,9 @@ let myList= ["eins","zwei", "drei"]
 
 function createHTMLList(liste) {
     // let ergebnis = "";
-    let htmlElements=liste.map((listItem) => `<li>${listItem}</li>`);
+    let htmlElements=liste.map((listItem, index) =>
+         `<li>${listItem}</li>`
+    );
     // console.log(htmlElements);
     let flatList=htmlElements.toString().replaceAll(",", "");
     // console.log(flatList);
@@ -19,10 +21,32 @@ function setListContent() {
     listDiv.innerHTML = content;
 }
 
+// Funktion, um eine ganze Liste, durch Kommata getrennt, hinzuzufügen.
 function setUserInputList() {
     let userInput = document.getElementById("userInput");
     let text = userInput.value;
     let textList = text.split(",");
     myList = myList.concat(textList);
     setListContent();
+}
+
+// Funktion, um einen einzelnen Eintrag hinzuzufügen.
+function soloInput() {
+    let single = document.getElementById("singleInput");
+    let newText = single.value;
+    myList.push(newText);
+    setListContent(); 
+    single.value = "";
+}
+
+// Funktion, die den letztewn Eintrag löscht.
+function killsLast() {
+    myList.pop();
+    setListContent();
+}
+
+// Funktion, die einzelne Elemente löschen kann.
+function deleteItem(index) {
+    myList.splice(index, 1);
+    document.getElementById("Liste").innerHTML = createHTMLList(myList);
 }
