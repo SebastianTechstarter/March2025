bb5cb971f2a1bd4045f594ef2a508a908e9dd56a
Beatbroetchen88 <sebastianrehberg.rehberg@tn.techstarter.de>
2025-03-11 16:47:18 +0100

    finish homework

--- HA-2025-03-11/Neues_HA_Paket/index.js
+++ HA-2025-03-11/Neues_HA_Paket/index.js
@@ -1,18 +1,36 @@
 import express from "express";
 const app = express();
 
+// Index-Pfad, der "Hello World" zurückgibt.
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-// gibt HTML als String zurück
+// gibt HTML als String zurück.
 app.get("/content", (req, res) => {
     res.send("<h1>HTML-Objekt</h1>");
 });
 
-// gibt ein JSON-Objekt zurück
+// gibt ein JSON-Objekt zurück.
 app.get("/api/data", (req, res) => {
-    res.json();
+    res.json([
+        {
+            "ID": "User1",
+            "name": "Sebastian", "age": 37,
+        },
+        {
+            "ID": "User2",
+            "name": "Christof", "age": 29,
+        },
+        {
+            "ID": "User3",
+            "name": "Mete", "age": 34,
+        },
+        {
+            "ID": "User4",
+            "name": "Joshua", "age": 31,
+        }
+    ]);
 });
 
 app.listen(3000, () => {
---
+++ HA-2025-03-11/Neues_HA_Paket/test.js
@@ -0,0 +1,20 @@
+let list = [
+    {
+        "ID": "User1",
+        "name": "Sebastian", "age": 37,
+    },
+    {
+        "ID": "User2",
+        "name": "Christof", "age": 29,
+    },
+    {
+        "ID": "User3",
+        "name": "Mete", "age": 34,
+    },
+    {
+        "ID": "User4",
+        "name": "Joshua", "age": 29,
+    }
+]
+
+console.log(list[1]);
